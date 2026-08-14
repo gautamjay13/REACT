b43eca995e4b80ea2ab48f2471dd4ed3485c76b5
jay gautam <jaygautamlu13@gmail.com>
2026-08-14 13:57:34 +0530

    project

---
+++ 01-react(basic project)/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Hero from "./Components/Hero";
+import Navigation from "./Components/Navigation"
+function App() {
+  return (
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App ;
